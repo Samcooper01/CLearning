42f292165534c5f18ef26f69e9eaf655dd6155d7
Samuel Cooper <Samuel.Cooper1@ge.com>
2023-02-17 10:12:58 -0600

    added macros and changed to for loop

--- temperatureConversion.c
+++ temperatureConversion.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
 int main() {
+    #define LOWER 0
+    #define UPPER 300
+    #define STEP 20
     float celc, farh;
-    int lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
 
     printf("Celcius to Fahrenheit Conversion\n");
-    celc = lower;
-    while(lower <= upper) {
+    for(celc = LOWER; celc <= UPPER; celc+=STEP) {
         farh = (celc * (9.0/5.0) + 32);
         printf("%3.0f\t%6.1f\n", celc, farh);
         celc += 20;
-        lower += step;
     }
 }
